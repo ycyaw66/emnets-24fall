64df18519ce11874ad69afdaa89268190b769cc2
林海龙 <linhl@emnets.org>
2024-05-27 14:41:04 +0800

    nimble gatt server

---
+++ 10_nimble_server/gatt_service.h
@@ -0,0 +1,139 @@
+#ifndef BLE_GATT_SVC_GAP  
+#define BLE_GATT_SVC_GAP                    (0x1800)    /**< Generic Access */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_GATT  
+#define BLE_GATT_SVC_GATT                   (0x1801)    /**< Generic Attribute */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_IA  
+#define BLE_GATT_SVC_IA                     (0x1802)    /**< Immediate Alert */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_LL  
+#define BLE_GATT_SVC_LL                     (0x1803)    /**< Link Loss */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_TP  
+#define BLE_GATT_SVC_TP                     (0x1804)    /**< Tx Power */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_CTS  
+#define BLE_GATT_SVC_CTS                    (0x1805)    /**< Current Time Service */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_RTUS  
+#define BLE_GATT_SVC_RTUS                   (0x1806)    /**< Reference Time Update Service */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_NDCS  
+#define BLE_GATT_SVC_NDCS                   (0x1807)    /**< Next DST Change Service */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_GLUCOSE  
+#define BLE_GATT_SVC_GLUCOSE                (0x1808)    /**< Glucose */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_HT  
+#define BLE_GATT_SVC_HT                     (0x1809)    /**< Health Thermometer */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_DEVINFO  
+#define BLE_GATT_SVC_DEVINFO                (0x180A)    /**< Device Information */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_HRS  
+#define BLE_GATT_SVC_HRS                    (0x180D)    /**< Heart Rate */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_PASS  
+#define BLE_GATT_SVC_PASS                   (0x180E)    /**< Phone Alert Status Service */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_BAS  
+#define BLE_GATT_SVC_BAS                    (0x180F)    /**< Battery Service */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_BPS  
+#define BLE_GATT_SVC_BPS                    (0x1810)    /**< Blood Pressure */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_ANS  
+#define BLE_GATT_SVC_ANS                    (0x1811)    /**< Alert Notification Service */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_HID  
+#define BLE_GATT_SVC_HID                    (0x1812)    /**< Human Interface Device */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_SP  
+#define BLE_GATT_SVC_SP                     (0x1813)    /**< Scan Parameters */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_RSC  
+#define BLE_GATT_SVC_RSC                    (0x1814)    /**< Running Speed and Cadence */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_AIO  
+#define BLE_GATT_SVC_AIO                    (0x1815)    /**< Automation IO */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_CSC  
+#define BLE_GATT_SVC_CSC                    (0x1816)    /**< Cycling Speed and Cadence */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_CP  
+#define BLE_GATT_SVC_CP                     (0x1818)    /**< Cycling Power */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_LN  
+#define BLE_GATT_SVC_LN                     (0x1819)    /**< Location and Navigation */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_ES  
+#define BLE_GATT_SVC_ES                     (0x181A)    /**< Environmental Sensing */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_BC  
+#define BLE_GATT_SVC_BC                     (0x181B)    /**< Body Composition */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_UD  
+#define BLE_GATT_SVC_UD                     (0x181C)    /**< User Data */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_WS  
+#define BLE_GATT_SVC_WS                     (0x181D)    /**< Weight Scale */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_BM  
+#define BLE_GATT_SVC_BM                     (0x181E)    /**< Bond Management */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_CGM  
+#define BLE_GATT_SVC_CGM                    (0x181F)    /**< Continuous Glucose Monitoring */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_IPS  
+#define BLE_GATT_SVC_IPS                    (0x1820)    /**< Internet Protocol Support */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_IP  
+#define BLE_GATT_SVC_IP                     (0x1821)    /**< Indoor Positioning */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_PO  
+#define BLE_GATT_SVC_PO                     (0x1822)    /**< Pulse Oximeter */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_HPS  
+#define BLE_GATT_SVC_HPS                    (0x1823)    /**< HTTP Proxy */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_TDS  
+#define BLE_GATT_SVC_TDS                    (0x1824)    /**< Transport Discovery */  
+#endif  
+  
+#ifndef BLE_GATT_SVC_OTS  
+#define BLE_GATT_SVC_OTS                    (0x1825)    /**< Object Transfer */  
+#endif  
